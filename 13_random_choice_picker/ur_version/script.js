000cce88c07a9635f6478b4f08f9edf5ba9d6e96
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-01-05 05:48:29 -0500

    Create tags from input

---
+++ 13_random_choice_picker/ur_version/script.js
@@ -0,0 +1,15 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tagsArray = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  console.log(tagsArray);
+}
