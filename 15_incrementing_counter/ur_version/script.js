e0fbdf48cc1b962347bfe77dc5e8229069a0bb67
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-01-10 05:24:27 -0500

    Modify script to match udemy implementation

--- 15_incrementing_counter/ur_version/script.js
+++ 15_incrementing_counter/ur_version/script.js
@@ -3,20 +3,22 @@ const counters = document.querySelectorAll(".counter");
 counters.forEach((counter) => {
   // set initial value of counter
   counter.innerText = "0";
+
+  const updateCounter = () => {
+    console.log("inside");
+    // get target number
+    const target = +counter.getAttribute("data-target");
+    const incr = target / 10;
+
+    let curVal = +counter.innerText;
+    if (curVal < target) {
+      curVal += incr;
+      counter.innerText = `${Math.ceil(curVal)}`;
+      setTimeout(updateCounter, 1000);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
   updateCounter();
-
-  const target = +counter.getAttribute("data-target");
-  const incr = target / 10;
-  let curVal = +counter.innerText;
-
-  function updateCounter() {
-    setTimeout((params) => {
-      console.log("running timeout");
-      if (curVal < target) {
-        curVal += incr;
-        counter.innerText = `${Math.ceil(curVal)}`;
-        updateCounter();
-      }
-    }, 1000);
-  }
 });
