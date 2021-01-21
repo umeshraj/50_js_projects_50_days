0718ec60bcc09aa76d908f6549eeb959aa2d9d01
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-01-21 05:48:34 -0500

    js for filling small cups

---
+++ 16_drink_water/udemy_version/script.js
@@ -0,0 +1,28 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => {
+    highlightCups(idx);
+  });
+});
+
+function highlightCups(idx) {
+  // toggle if current full and clicked
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
