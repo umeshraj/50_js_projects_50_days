a90485f80db53393f116677dcfcf79d3a5ea0d3c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-06 05:16:40 -0500

    Add date, day, month

--- 19_theme_clock/ur_version/script.js
+++ 19_theme_clock/ur_version/script.js
@@ -5,6 +5,30 @@ const dateEl = document.querySelector(".date");
 const timeEl = document.querySelector(".time");
 const toggleEl = document.querySelector(".toggle");
 
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 toggleEl.addEventListener("click", (e) => {
   const html = document.querySelector("html");
   if (html.classList.contains("dark")) {
@@ -26,6 +50,15 @@ function setTime() {
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
 
-  const hoursForClock = hourEl % 12;
+  const hoursForClock = hours % 12;
   const ampm = hours >= 12 ? "PM" : "AM";
+
+  // set time
+  timeEl.innerHTML = `${hoursForClock}: ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+  // set date
+  dateEl.innerHTML = `${days[day]} ${months[month]} <span class="circle">${date}</span>`;
 }
+
+setTime();
