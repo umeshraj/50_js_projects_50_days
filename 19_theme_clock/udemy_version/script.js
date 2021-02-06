c6da849b7d79d3f1fc595c58a3efba9cbad9c19f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-06 05:26:26 -0500

    Fix unreachable code

--- 19_theme_clock/udemy_version/script.js
+++ 19_theme_clock/udemy_version/script.js
@@ -83,10 +83,6 @@ function setTime() {
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-
-  timeEl.innerHTML = `${hoursForClock}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  } ${ampm}`;
 };
 
 setTime();
