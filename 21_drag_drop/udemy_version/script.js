848ffba180b969b89255ab16c79f656f3bfc5a37
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-10 05:25:52 -0500

    Adding js to handle drag and drop

---
+++ 21_drag_drop/udemy_version/script.js
@@ -0,0 +1,41 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  console.log(this);
+  this.className += " hold";
+  setTimeout(() => {
+    this.className = "invisible";
+  }, 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {
+  console.log("Drag leave");
+}
+
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}
