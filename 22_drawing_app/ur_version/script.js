d12e9625eb529c85c7a443f1396756a60c35806b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-15 05:22:56 -0400

    Change line props

--- 22_drawing_app/ur_version/script.js
+++ 22_drawing_app/ur_version/script.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("canvas");
+const colorEl = document.getElementById("color");
+const sizeEl = document.getElementById("size");
+const increaseEl = document.getElementById("increase");
+const decreaseEl = document.getElementById("decrease");
 
 const ctx = canvas.getContext("2d");
 let isPressed = false;
@@ -24,17 +28,45 @@ canvas.addEventListener("mousemove", (e) => {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
 
+    drawCircle(x, y);
     drawLine(x, y, x2, y2);
     x = x2;
     y = y2;
   }
 });
 
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
 function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.linewidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size;
+}
+
+colorEl.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+increaseEl.addEventListener("click", () => {
+  size += 5;
+  size = size > 50 ? 50 : size;
+  updateSizeOnScreen();
+});
+
+decreaseEl.addEventListener("click", () => {
+  size -= 5;
+  size = size <= 0 ? 5 : size;
+  updateSizeOnScreen();
+});
