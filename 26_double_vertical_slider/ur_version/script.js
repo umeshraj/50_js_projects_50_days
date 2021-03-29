dfe1ddc2a20ab5805a0db8f93e0ad65c66a96fa7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-29 05:17:37 -0400

    Handle translations on click

---
+++ 26_double_vertical_slider/ur_version/script.js
@@ -0,0 +1,42 @@
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+
+const sliderContainer = document.querySelector(".slider-container");
+const slideLeft = document.querySelector(".left-slide");
+const slideRight = document.querySelector(".right-slide");
+
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+downButton.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction == "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction == "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  console.log(`translateY(-${activeSlideIndex * sliderHeight}px)`);
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
