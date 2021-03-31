6dff6bf8daa5c896c498064b513309f7204054b7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-03-31 05:37:57 -0400

    Add notification on button press

---
+++ 27_toast_notification/ur_version/script.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+button.addEventListener("click", () => {
+  createNotification();
+});
+
+function createNotification() {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.innerText = getRandomMessage();
+
+  toasts.appendChild(notif);
+}
+
+function getRandomMessage() {
+  const messages = [
+    "Message One",
+    "Message Two",
+    "Message Three",
+    "Message Four",
+  ];
+
+  return messages[Math.floor(Math.random() * messages.length)];
+}
