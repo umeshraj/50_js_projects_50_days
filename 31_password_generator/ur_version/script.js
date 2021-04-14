47b43034a2ff091e9bad0b36f4502510496938a2
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-04-14 05:06:06 -0400

    Generate random password and copy

--- 31_password_generator/ur_version/script.js
+++ 31_password_generator/ur_version/script.js
@@ -7,7 +7,65 @@ const symbolsEl = document.getElementById("symbols");
 const generateEl = document.getElementById("generate");
 const clipboardEl = document.getElementById("clipboard");
 
-function generateLower() {
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+clipboardEl.addEventListener("click", () => {
+  const password = resultEl.innerText;
+  if (!password) {
+    return;
+  }
+  const textarea = document.createElement("textarea");
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password has been copied");
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let finalPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  if (typesCount == 0) {
+    return "";
+  }
+
+  const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArray.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      finalPassword += randomFunc[funcName]();
+    });
+  }
+  const trimPassword = finalPassword.slice(0, length);
+  return trimPassword;
+}
+
+function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
