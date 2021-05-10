19070a04d78044f53870e916ff67c72c8c1251a6
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-05-10 05:09:06 -0400

    Wrapping up for js for ur version of feedback

---
+++ 43_feedback_ui/ur_version/script.js
@@ -0,0 +1,31 @@
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendBtn = document.getElementById("send");
+const panel = document.getElementById("panel");
+
+let selectedRating = "Satisfied";
+
+ratingsContainer.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+    selectedRating = e.target.nextElementSibling.innerHTML;
+    console.log(selectedRating);
+  }
+});
+
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `
+        <i class = "fas fa-heart"></i>
+        <strong>Thank you </strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We will use your feedback to improve</p>
+    `;
+});
+
+function removeActive() {
+  ratings.forEach((rating) => {
+    rating.classList.remove("active");
+  });
+}
