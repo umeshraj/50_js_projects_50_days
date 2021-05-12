73b372fbc5b383d72cacaf631fcea3b15f9c7665
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-05-12 04:56:24 -0400

    Error in copy/paste

--- 44_custom_range_slider/ur_version/script.js
+++ 44_custom_range_slider/ur_version/script.js
@@ -8,7 +8,7 @@ range.addEventListener("input", (e) => {
   const label_width = getComputedStyle(label).getPropertyValue("width");
 
   const num_width = +range_width.substring(0, range_width.length - 2);
-  const num_label_width = +range_width.substring(0, label_width.length - 2);
+  const num_label_width = +label_width.substring(0, label_width.length - 2);
 
   const max = +e.target.max;
   const min = +e.target.min;
